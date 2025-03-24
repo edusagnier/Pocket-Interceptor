1c7ace3a375e3863289012bdcda11a576a3fac9d
Eduard Sagnier <colasagnier@gmail.com>
2025-03-24 23:04:28 +0100

    Solved logial issue

--- netdisc.sh
+++ netdisc.sh
@@ -11,15 +11,15 @@ menu() {
     case "$choice" in
         1)
             echo "Scanning the entire network..."
-            sudo 600s timeout netdiscover -PN > data_collected/netmapping/scanall.txt
+            sudo timeout 750 netdiscover -PN > data_collected/netmapping/scanall.txt
             ;;
         2)
             read -p "Enter the network (Ex: 192.168.1.0/24): " network
-            sudo netdiscover -r 192.168.3.0/24 -PN > data_collected/netmapping/scan.txt
+            sudo timeout 300 tnetdiscover -r 192.168.3.0/24 -PN > data_collected/netmapping/scan.txt
             ;;
         3)
             echo "Exiting..."
-            exit(1)
+            exit
             ;;
         *)
             echo "Invalid choice! Please enter 1, 2, 3, or 4"
